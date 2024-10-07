2a883a601ae4d9d695144291cf964da459ff4477
akonyahin <konyahin_al@mail.ru>
2024-10-07 22:55:38 +0300

    inertia test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,19 +8,22 @@ use Inertia\Inertia;
 class MainController extends Controller
 {
     public function index()
-    {
-        return view('welcome');
-    }
+    // {
+    //     return view('welcome');
+    // }
     // public function show(Event $event)
-    public function show($event)
+    // public function show($event)
     {
-        return Inertia::render('Event/Show', [
-            'event' => $event->only(
-                'id',
-                'title',
-                'start_date',
-                'description'
-            ),
+        return Inertia::render('App/App', [
+            'user' => [
+                'name' => 'Jhon Doe',
+            ],
+            // 'event' => $event->only(
+            //     'id',
+            //     'title',
+            //     'start_date',
+            //     'description'
+            // ),
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   </head>
-  @vite('resources/js/index.jsx')
-  @inertiaHead
   <body>
-    @inertia
+    hello
   </body>
 </html>
