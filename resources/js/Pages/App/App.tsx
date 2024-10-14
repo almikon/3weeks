bc92ba518b88598947110d1b8cbe5c434ee10b22
akonyahin <konyahin_al@mail.ru>
2024-10-14 23:32:41 +0300

    router fix, test

--- resources/js/Pages/App/App.tsx
+++ resources/js/Pages/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Head } from '@inertiajs/react'
 import Block from './components/Block';
 import '../../../css/app.scss';
@@ -9,25 +9,41 @@ interface Props {
   city_name: string;
 }
 
+const getForecast = async (currentDate: Date, city: string) => {
+  return await fetch('/api/weather', {
+    method: 'POST',
+    body: JSON.stringify({
+      date: currentDate,
+      city
+    })
+  });
+}
+
 const App: FC<Props> = (props: Props) => {
-  const [result, setResult] = useState(props.data);
+  const [result, setResult] = useState(false);
+
+  const handleSubmit = async () => {
+    const response = await getForecast(new Date(), 'Москва');
+  };
 
   return (
     <>
     <Head title="Прогноз погоды на ближайшие 2 дня. Москва и область." />
     <div className="wrapper">
       <CitySelector />
-      <h1>{props.city_name}</h1>
-      <div className="container">
-        {props.data.map((item, index) => (
-          <Block
-            key={index}
-            date={item.date}
-            icon={item.day.condition.icon}
-            temperature={item.day.avgtemp_c}
-          />
-        ))}
-      </div>
+      {result && <div>
+        <h1>{props.city_name}</h1>
+        <div className="container">
+          {props.data.map((item, index) => (
+            <Block
+              key={index}
+              date={item.date}
+              icon={item.day.condition.icon}
+              temperature={item.day.avgtemp_c}
+            />
+          ))}
+        </div>
+      </div>}
     </div>
     </>
   )
